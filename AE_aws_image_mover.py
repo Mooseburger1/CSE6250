e4c4217628b006ee5bd16b4e8e4c4156c6f303cc
yodanater <danems11@gmail.com>
2019-11-16 15:46:25 -0700

    modified for aws script to put images in correct locations, 30% images go to valid folder, 70% go to train

--- AE_aws_image_mover.py
+++ AE_aws_image_mover.py
@@ -1,6 +1,7 @@
 # How to run script
-# python AE_image_mover.py file.csv 
+# python AE_image_mover.py file.csv bucket
 # file.csv is the file to transfer images from
+# bucket is the name of s3 bucket
 # in this case all the files are in autoencoders folder so I have
 # that hard coded below
 import pandas as pd
@@ -16,40 +17,41 @@ aws_secret_key_id = "tJ4DEIYjJ3a86gdzMBcVc+Qn2DgSoRflx7WKH+Z6"
 s3 = boto3.resource('s3')
 
 copy_source = {
-    'Bucket': 'mybucket',
-    'Key': 'mykey'
+    'Bucket': 'Bucket',
+    'Key': 'folder/training/images.csv'
 }
 
 csv_src = sys.argv[1]
+bucket = sys.argv[2]
 
 new_folder = sys.argv[1].split(".")[0]
-try:
-    os.rmdir(new_folder)
-except:
-    pass
-try:
-    os.mkdir(new_folder)
-except:
-    pass
+
 
 #df = pd.read_csv("Pleural_Other.csv")
 df = pd.read_csv("autoencoders/"+str(csv_src)) # hard coded to be in this folder, can change this if we want
 count_f = 0
 count_l = 0
 for path in df['Path']:
+    rand = np.random.randint(1,10)
+    if rand < 4:
+        pre = 'valid/'
+    else:
+        pre = 'train/'
     img_name = path.split("/")[-1]
     if "frontal" in img_name:
-        name = "frontal_"+str(count_f)+".jpg"
+        name = "/frontal_"+str(count_f)+".jpg"
         count_f +=1
     else:
-        name = "lateral_"+str(count_l)+".jpg"
+        name = "/lateral_"+str(count_l)+".jpg"
         count_l +=1
 
     copy_source = {
-        'Bucket': path,
-        'Key': aws_access_key_id
+        'Bucket': bucket,
+        'Key': path  # might need to modify this depending on how we setup the s3 storage
     }
-    s3.meta.client.copy(copy_source, new_folder+"/"+name, aws_access_key_id)
+    s3.meta.client.copy(copy_source, bucket, pre + new_folder+name)
     #copyfile(path,new_folder+"/"+name) # python way to copy new file 
     
     #print(path)
+    # secret key
+    #eIDnM0x1FbhHQxCLlGEhvftkq5WM2AqklPtJEPie
